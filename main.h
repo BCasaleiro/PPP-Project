ff6d44ef5f0f492319ffaa6d4097b739ab076e2f
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2014-05-07 12:42:41 +0100

    changed the output of verifica

--- main.h
+++ main.h
@@ -3,4 +3,5 @@
 void menu(reservas lista_reservas, prereservas lista_pre);
 void reserva(reservas lista_reservas, prereservas lista_pre);
 void reservar(reservas lista_reservas, prereservas lista_pre, char op);
+int verifica_vaga(reservas lista_reservas, char op, int dia, int mes, int ano);
 void clear_screen();
